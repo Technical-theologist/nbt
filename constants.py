921fb0fbfff94d43ee815a54e34658f3171329ef
karthikeya <karthikeya@gosimple.in>
2024-12-18 20:20:35 +0530

    venv files

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
 class Constants():
-    url = "https://nbt.gosimple.io/home"
-    homeUrl = "https://nbt.gosimple.io/home"
+    url = "https://nbt.iotalchemy.com/home"
+    homeUrl = "https://nbt.iotalchemy.com/home"
     loginEmail = "karthikeya.sarma95@gmail.com"
     loginPassword = "Nbt@123"
 
-    apiBaseUrl = "https://nbt.gosimple.io:8089/api/v1/"
+    apiBaseUrl = "https://nbt.iotalchemy.com:8089/api/v1/"
     productsApi = apiBaseUrl+"productinfo"
     tagsApi = apiBaseUrl+"tags"
